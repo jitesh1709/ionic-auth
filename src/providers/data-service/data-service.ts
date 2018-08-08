05c0367b284dd597564e480a265467f6f60dd2c1
jitesh1709 <jiteshsingh1709@gmail.com>
2018-08-08 21:12:35 +0530

    added bootstarp

--- src/providers/data-service/data-service.ts
+++ src/providers/data-service/data-service.ts
@@ -1,29 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from "rxjs/operators";
-import 'rxjs/add/operator/map';
 
+import 'rxjs/add/operator/map';
 @Injectable()
 export class DataServiceProvider {
-	items:any;	
+	result:any;	
 	// url='/assets/response.json'; 
-  constructor(public http: HttpClient) {
-  	this.items = [
-  	{title:'one'},
-  	{title:'two'},
-  	{title:'three'},
-  	{title:'four'},
-  	{title:'five'},
-  	{title:'six'}
-  	]
-  }
- //  getUsers(){
- //  	return this.http.get(this.url)
-	// 	.pipe(map(result => this.result = <any>result)); 
-	// } 
-	filterUsers(searchTerm){
-		return this.items.filter((item)=>{
-			return item.title.toLowerCase().indexOf(searchTerm.toLowerCase())>-1;
-		});
-	}
+  constructor(public http: HttpClient) {}
+//   getUsers(){
+//   	return this.http.get(this.url)
+// 		.pipe(map(result => this.result = <any>result)); 
+// 	} 
 }
