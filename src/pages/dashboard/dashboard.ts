8a707c30dd587f78bc3b1c2c2b6829a43b714e5b
jitesh1709 <jiteshsingh1709@gmail.com>
2018-08-08 21:11:33 +0530

    UI enhancement, pagination has been provided, showing more details about user

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { DataServiceProvider } from '../../providers/data-service/data-service';
+import { PageProvider } from '../../providers/page/page';
 import 'rxjs/add/operator/debounceTime';
 
 @IonicPage()
@@ -11,37 +13,34 @@ import 'rxjs/add/operator/debounceTime';
   providers : [DataServiceProvider]
 })
 export class DashboardPage implements OnInit {
-  items:any;
-  searchTerm:string;
-  searchControl:FormControl;
-  searching:any=false;
-
+  private users:any[];
+  pagedItems:any[];
+  pager:any={};
+  url='assets/response.json'; 
   ngOnInit(){
-    // this.dataService.getUsers()
-    //   .subscribe(result => this.users = <any>result);
+    //setting data
+    this.http.get(this.url)
+      .map((response:Response)=>response.json())
+        .subscribe(result =>{ 
+        //setting data
+        this.users =result;
+        
+        //setting to page 1
+        this.setPage(1);
+        });
   }
-
   constructor(
     public navCtrl: NavController, 
-    public navParams: NavParams, 
-    private dataService : DataServiceProvider) {
-    this.searchControl = new FormControl();
-  }
-
-  ionViewDidLoad(){
-    this.setFilteredItems();
-    this.searchControl.valueChanges.debounceTime(700)
-      .subscribe(search =>{
-        this.searching = false;
-        this.setFilteredItems();
-      });
-  }
-
-  onSearchInput(){
-    this.searching = true;
-  }
-
-  setFilteredItems(){
-    this.items = this.dataService.filterUsers(this.searchTerm);
+    public http:Http, 
+    private dataService : DataServiceProvider,
+    private pageService: PageProvider
+  ) {}
+  setPage(page:number){
+    
+    //getting page object from service
+    this.pager = this.pageService.getPager(this.users.length,page);
+    
+    //get current page of items
+    this.pagedItems = this.users.slice(this.pager.startIndex, this.pager.endIndex+1);
   }
 }
